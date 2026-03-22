efce6c3b9e0242f5291e548b327a42b11e0bde6b
takayama <takashi.gashira@gmail.com>
2026-03-22 15:24:00 +0900

    並行処理実装

--- demon_server.cpp
+++ demon_server.cpp
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <cstring>
 #include <fstream>
+#include <thread>
 
 class Character {
 public:
@@ -26,40 +27,13 @@ public:
 Character hero(100);
 Character demon(300);
 
-int main() {
-    std::ifstream load_file("save.txt");
-
-    // ファイルが無事に開けたか（前回セーブしたか）を確認
-    if (load_file.is_open()) {
-    load_file >> hero.hp >> demon.hp >> demon.is_poisoned;
-    load_file.close();
-
-    std::cout << "【システム】前回のセーブデータを読み込みました！\n";
-        std::cout << "復元HP -> 勇者: " << hero.hp << " / 魔王: " << demon.hp << "\n";
-    } else {
-        std::cout << "【システム】セーブデータがありません。初めからスタートします。\n";
-    }
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
-
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 3);
-
-    std::cout << "魔王（サーバー）起動：ポート8080で待機中...\n";
-    int new_socket = accept(server_fd, nullptr, nullptr);
-    std::cout << "勇者が接続してきた！戦闘開始！\n";
+void handle_hero(int client_socket) {
 
     char buffer[1024] = {0};
 
     while (true) {
         memset(buffer, 0, sizeof(buffer));
-        int valread = read(new_socket, buffer, 1024);
+        int valread = read(client_socket, buffer, 1024);
         if (valread <= 0) {
             std::cout << "勇者が通信を切断した。\n";
             break;
@@ -111,13 +85,56 @@ int main() {
         result_msg += "現在のHP -> 勇者: " + std::to_string(hero.hp) + " / 魔王: " + std::to_string(demon.hp);
 
         // クライアント（勇者）に結果を送信
-        send(new_socket, result_msg.c_str(), result_msg.length(), 0);
-        
+        send(client_socket, result_msg.c_str(), result_msg.length(), 0);
+
         // サーバー側の画面にも状況を表示
         std::cout << "処理完了: 勇者HP=" << hero.hp << " 魔王HP=" << demon.hp << "\n";
     }
+        close(client_socket);
 
-    close(new_socket);
+}
+
+int main() {
+    std::ifstream load_file("save.txt");
+
+    // ファイルが無事に開けたか（前回セーブしたか）を確認
+    if (load_file.is_open()) {
+    load_file >> hero.hp >> demon.hp >> demon.is_poisoned;
+    load_file.close();
+
+    std::cout << "【システム】前回のセーブデータを読み込みました！\n";
+        std::cout << "復元HP -> 勇者: " << hero.hp << " / 魔王: " << demon.hp << "\n";
+    } else {
+        std::cout << "【システム】セーブデータがありません。初めからスタートします。\n";
+    }
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    std::cout << "魔王（マルチスレッド対応版）起動：ポート8080で待機中...\n";
+
+    // 受付係の無限ループ
+    while (true) {
+        // 1. 勇者が来るのを待つ（ここで一時停止して待機する）
+        int new_socket = accept(server_fd, nullptr, nullptr);
+        std::cout << "勇者が接続してきた！戦闘開始！\n";
+        std::cout << "【システム】新しい勇者が魔王の城に侵入しました！\n";
+
+        // 2. 【究極のハッキング】案内係（スレッド）を生み出して丸投げする！
+        // std::thread(実行したい関数, 渡したいデータ).detach();
+        std::thread(handle_hero, new_socket).detach();
+
+        // 分身を生み出したら、受付係はすぐにループの先頭に戻り、次の勇者を待つ！
+    }
+
     close(server_fd);
     return 0;
 }
